f3ca4d44494e9bea9dd0d3c2099b38e1e12693e1
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-05-27 10:17:26 -0400

    Successful webpack setup

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const PATH = require('path')
 const app = express()
 const port = 3000
 
-app.use(express.static(PATH.join(__dirname, 'client', 'src')))
+app.use(express.static(PATH.join(__dirname, 'client', 'dist')))
 
-app.get('/', (req, res) => {
-  //res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
