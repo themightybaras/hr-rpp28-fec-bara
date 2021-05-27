6d875ddeac97542e941852fd1f3ac4736ee1cb5e
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-05-27 00:41:21 -0400

    Revised module rules test to only look for jsx files, imported path and revised entry/output options to use join instead of resolve

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
+const path = require('path');
+
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.join(__dirname, 'src', 'index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.jsx$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
       }
