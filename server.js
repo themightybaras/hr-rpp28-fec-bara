88dcc3c8ad50609085fa80e20386e0d73b34606f
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-05-30 16:40:09 -0400

    Added semicolons to server commands

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const PATH = require('path')
-const app = express()
-const port = 3000
+const express = require('express');
+const PATH = require('path');
+const app = express();
+const port = 3000;
 
-app.use(express.static(PATH.join(__dirname, 'client', 'dist')))
+app.use(express.static(PATH.join(__dirname, 'client', 'dist')));
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
