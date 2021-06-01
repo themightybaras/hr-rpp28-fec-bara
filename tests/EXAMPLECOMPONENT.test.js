9b19b6563139ca512017427375ab321d69f2c219
Sara Landis <saralandis5555@gmail.com>
2021-06-01 19:22:23 -0400

    Moved test file to root of repo

--- tests/EXAMPLECOMPONENT.test.js
+++ tests/EXAMPLECOMPONENT.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({adapter: new Adapter()});
-import ExampleComponent from '../ExampleTestComponent.jsx';
+import ExampleComponent from '../client/src/ExampleTestComponent.jsx';
 
 test('ExampleComponent renders correct text', ()=>{
   const exampleComponent = shallow(
