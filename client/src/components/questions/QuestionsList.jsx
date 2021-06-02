8f5fde52c86ccd744b6df310eca19128083a34a3
Jennifer Yang <jennifer.jinyoung.yang@gmail.com>
2021-06-01 22:08:21 -0400

    [WIP] Create QuestionsList component

---
+++ client/src/components/questions/QuestionsList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class QuestionsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Questions & Answers</h3>
+      </div>
+    );
+  }
+}
+
+export default QuestionsList;
+
