0e7033241b8edc5fa209e66cbc2d48f09d763c45
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-06-01 23:29:06 -0400

    Created container component for related products and outfit

---
+++ client/src/components/related/Related.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Main container for all components related to related products and outfit
+// this component will be called in App.jsx
