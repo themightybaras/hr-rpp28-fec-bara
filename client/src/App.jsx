0272ffa2eb4f2e3c658247cc0b5d2ac1b987047d
Chelsea Lipson <chelseamlipson@gmail.com>
2021-06-03 17:27:29 -0700

    Completed basic app outline.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Overview from './components/overview/Overview.jsx';
 import RelatedOutfit from './components/related/RelatedOutfit.jsx';
 import QuestionsList from './components/questions/QuestionsList.jsx';
+import ReviewWidget from './components/review/ReviewWidget.jsx';
 
 class App extends React.Component {
   constructor (props) {
@@ -18,6 +19,7 @@ class App extends React.Component {
         <Overview/>
         <RelatedOutfit/>
         <QuestionsList/>
+        <ReviewWidget/>
       </div>
     );
   }
