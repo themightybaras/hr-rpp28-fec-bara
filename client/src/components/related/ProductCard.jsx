a854c9c18dddcc7faa70e727fc8da1d4a02cfbef
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-06-05 19:32:29 -0400

    Commented out Outfit caorusel. Refactored ProductCard to use separate components.

--- client/src/components/related/ProductCard.jsx
+++ client/src/components/related/ProductCard.jsx
@@ -2,31 +2,62 @@ import React from 'react';
 
 // takes in product info
 
+// Individual card components
+let ActionItem = () => {
+  return (
+    <span>
+        Action Item
+    </span>
+  );
+};
+let Image = () => {
+  return (
+    <img className='relatedImage' src='http://d.ibtimes.co.uk/en/full/429795/13-year-old-norwegian-magnus-carlsen-concentrates-during-match-belarus-player-alexei-fedorov.jpg'></img>
+  );
+};
+let Category = () => {
+  return (
+    <span>
+      category
+    </span>
+  );
+};
+let Name = () => {
+  return (
+    <span>
+      <strong>Name</strong>
+    </span>
+  );
+};
+let Price = () => {
+  return (
+    <span>
+      price
+    </span>
+  );
+};
+let Review = () => {
+  return (
+    <span>
+      review
+    </span>
+  );
+};
 // renders image, info, action button
 const ProductCard = () => {
   return (
     <div className="productcard">
-      <span>
-        Action Item
-      </span>
-      <br></br>
-      <img className='relatedImage' src='http://d.ibtimes.co.uk/en/full/429795/13-year-old-norwegian-magnus-carlsen-concentrates-during-match-belarus-player-alexei-fedorov.jpg'></img>
-      <br></br>
-      <span>
-        category
-      </span>
-      <br></br>
-      <span>
-        <strong>name</strong>
-      </span>
-      <br></br>
-      <span>
-        price
-      </span>
-      <br></br>
-      <span>
-        review
-      </span>
+      <ActionItem />
+      <br />
+      <Image />
+      <br />
+      <Category />
+      <br />
+      <Name />
+      <br />
+      <Price />
+      <br />
+      <Review />
     </div>
   );
 };
