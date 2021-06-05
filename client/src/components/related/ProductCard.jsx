d21dc1b329977dc45c84861832176e9da8f0035e
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-06-04 23:23:53 -0400

    Added placeholder in product card for action item

--- client/src/components/related/ProductCard.jsx
+++ client/src/components/related/ProductCard.jsx
@@ -6,6 +6,9 @@ import React from 'react';
 const ProductCard = () => {
   return (
     <div className="productcard">
+      <div>
+        Action Item
+      </div>
       <img className='relatedImage' src='http://d.ibtimes.co.uk/en/full/429795/13-year-old-norwegian-magnus-carlsen-concentrates-during-match-belarus-player-alexei-fedorov.jpg'></img>
       <div>
         category
