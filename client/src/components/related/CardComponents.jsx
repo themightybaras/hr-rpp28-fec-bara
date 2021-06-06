e33b95da6138ccacab5742fa8ac95d015d67e063
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-06-05 21:00:23 -0400

    Moved individual product card components into separate React module

---
+++ client/src/components/related/CardComponents.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+// Individual card components
+const ActionItem = () => {
+  return (
+    <span>
+        Action Item
+    </span>
+  );
+};
+const Image = () => {
+  return (
+    <img className='relatedImage' src='http://d.ibtimes.co.uk/en/full/429795/13-year-old-norwegian-magnus-carlsen-concentrates-during-match-belarus-player-alexei-fedorov.jpg'></img>
+  );
+};
+const Category = () => {
+  return (
+    <span>
+      category
+    </span>
+  );
+};
+const Name = () => {
+  return (
+    <span>
+      <strong>Name</strong>
+    </span>
+  );
+};
+const Price = () => {
+  return (
+    <span>
+      price
+    </span>
+  );
+};
+const Review = () => {
+  return (
+    <span>
+      review
+    </span>
+  );
+};
+
+export { ActionItem, Image, Category, Name, Price, Review };
--- client/src/components/related/ProductCard.jsx
+++ client/src/components/related/ProductCard.jsx
@@ -1,48 +1,9 @@
 import React from 'react';
+import {ActionItem, Image, Category, Name, Price, Review} from './CardComponents.jsx';
 
 // takes in product info
 
-// Individual card components
-let ActionItem = () => {
-  return (
-    <span>
-        Action Item
-    </span>
-  );
-};
-let Image = () => {
-  return (
-    <img className='relatedImage' src='http://d.ibtimes.co.uk/en/full/429795/13-year-old-norwegian-magnus-carlsen-concentrates-during-match-belarus-player-alexei-fedorov.jpg'></img>
-  );
-};
-let Category = () => {
-  return (
-    <span>
-      category
-    </span>
-  );
-};
-let Name = () => {
-  return (
-    <span>
-      <strong>Name</strong>
-    </span>
-  );
-};
-let Price = () => {
-  return (
-    <span>
-      price
-    </span>
-  );
-};
-let Review = () => {
-  return (
-    <span>
-      review
-    </span>
-  );
-};
+
 // renders image, info, action button
 const ProductCard = () => {
   return (
