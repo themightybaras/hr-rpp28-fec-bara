17a06b656d048a1dd12ef0e4ee728cf74c00e250
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-06-05 21:25:16 -0400

    Added pseudocode and import statements for related products router

---
+++ controllers/relatedRequests.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const APIKey = require('../config.js');
+const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp';
+
+/**
+ * Image: GET /products/:product_id/styles
+ * Category: GET /products/:product_id
+ * Name: GET /products/:product_id
+ * Price: GET /products/:product_id/styles
+ * Review: ? - NOT IN ANY RETURNED PRODUCTS OBJECT. ASK SARA WHICH API REQUEST (RATING) MAKES SENSE
+ * features: GET /products/:product_id
+ */
+
+// Get related products (returns array of product IDs)
+// How do we select a style for the related product - documentation says defautl style
+// For each related product (id)
+//    Map ID to promisified GET request (maybe multiple, depending on above)
+// Use Promise.all() to wait for all resolved API calls for additional product info
+//  Send response data
