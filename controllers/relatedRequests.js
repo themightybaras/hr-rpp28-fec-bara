1eaed207e816c549fda11f063c5a6abd26c5f045
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-06-05 23:21:33 -0400

    Added additional nested API call for basic product info for each related product (using example product id)

--- controllers/relatedRequests.js
+++ controllers/relatedRequests.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const Promise = require('bluebird');
 const APIKey = require('../config.js');
 const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp';
 
@@ -11,21 +12,32 @@ const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp';
  * features: GET /products/:product_id
  */
 
-// Get related products (returns array of product IDs)
 // How do we select a style for the related product - documentation says defautl style
-// For each related product (id)
-//    Map ID to promisified GET request (maybe multiple, depending on above)
-// Use Promise.all() to wait for all resolved API calls for additional product info
-//  Send response data
 
 const getRelated = (req, res) => {
   axios.defaults.headers.common['Authorization'] = APIKey;
+  // Get related products (returns array of product IDs)
   axios({
     method: 'get',
     url: baseURL + '/products/22126/related'
   })
     .then((response) => {
-      res.send(response.data);
+    // For each related product (id)
+    //    Map ID to promisified GET request (maybe multiple, depending on above)
+      let stdProdCalls = response.data.map((id) => {
+        return axios({
+          method: 'get',
+          url: `${baseURL}/products/${id}`
+        })
+          .then((response) => {
+            return response.data;
+          });
+      });
+      // Use Promise.all() to wait for all resolved API calls for additional product info
+      Promise.all(stdProdCalls).then((products) => {
+        res.send(products);
+      });
+      //  Send response data
     })
     .catch((err) => {
       res.sendStatus(404);
