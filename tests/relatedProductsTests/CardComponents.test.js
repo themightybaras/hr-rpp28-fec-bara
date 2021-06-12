a6f24c428f8c122cc359e007d6f500c886f2be77
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-06-12 14:46:03 -0400

    Added new file for CardComponent tests with two tests for Name and Category components

---
+++ tests/relatedProductsTests/CardComponents.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {shallow, configure, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({adapter: new Adapter()});
+
+import RelatedOutfit from '../../client/src/components/related/RelatedOutfit.jsx';
+import ProductCard from '../../client/src/components/related/ProductCard.jsx';
+import ItemsList from '../../client/src/components/related/ItemsList.jsx';
+import {ActionItem, Image, Category, Name, Price, Review} from '../../client/src/components/related/CardComponents.jsx';
+
+test('Simple text components render text passed as a prop', () => {
+  let testText = 'Test';
+  let testCategory = shallow(
+    <Category category={testText}/>
+  );
+  let testName = shallow(
+    <Name name={testText}/>
+  );
+  expect(testCategory.text()).toEqual(testText);
+  expect(testName.text()).toEqual(testText);
+});
