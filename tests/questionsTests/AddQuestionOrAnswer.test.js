fde0efec7a8e12d598b6c8df79df273d92a3a725
Jennifer Yang <jennifer.jinyoung.yang@gmail.com>
2021-06-21 00:37:04 -0400

    Complete 'add a question' and 'add an answer' functionality except for 'upload photos' functionality for 'add an answer' - edited AddQuestionOrAnswer test since the script to be tested has been modified

--- tests/questionsTests/AddQuestionOrAnswer.test.js
+++ tests/questionsTests/AddQuestionOrAnswer.test.js
@@ -6,6 +6,25 @@ import AddQuestionOrAnswer from '../../client/src/components/questions/AddQuesti
 
 test('test add question or answer block', () => {
 
+  const questionSample = {
+    question_id: 153695,
+    question_body: 'Why is this product cheaper here than other sites?',
+    question_date: '2018-10-18T00:00:00.000Z',
+    asker_name: 'willsmith',
+    question_helpfulness: 4,
+    reported: false,
+    answers: {
+      1444586: {
+        id: 1444586,
+        body: 'We are selling it here without any markup from the middleman!',
+        date: '2018-08-18T00:00:00.000Z',
+        answerer_name: 'Seller',
+        helpfulness: 4,
+        photos: []
+      }
+    }
+  };
+
   const addQuestionBlockNoModal = shallow(
     <AddQuestionOrAnswer addModalOpen={false} toggleAddModal={() => {}} isQuestionModal={true} />
   );
@@ -15,17 +34,17 @@ test('test add question or answer block', () => {
   );
 
   const addAnswerBlock = shallow(
-    <AddQuestionOrAnswer addModalOpen={true} toggleAddModal={() => {}} isQuestionModal={false} />
+    <AddQuestionOrAnswer question={questionSample} addModalOpen={true} toggleAddModal={() => {}} isQuestionModal={false} />
   );
 
   expect(addQuestionBlockNoModal.find('.display-none').exists()).toBe(true);
-  expect(addQuestionBlock.find('.add-your-question-or-answer-label').text()).toBe('Your Question');
-  expect(addQuestionBlock.find('.add-your-nickname-label').text()).toBe('What is Your Nickname');
-  expect(addQuestionBlock.find('.add-your-email-label').text()).toBe('Your Email');
+  expect(addQuestionBlock.find('.add-your-question-or-answer-label').text()).toBe('Your Question*');
+  expect(addQuestionBlock.find('.add-your-nickname-label').text()).toBe('What is Your Nickname*');
+  expect(addQuestionBlock.find('.add-your-email-label').text()).toBe('Your Email*');
   expect(addQuestionBlock.find('.add-question-or-answer-submit').text()).toBe('Submit Question');
-  expect(addAnswerBlock.find('.add-your-question-or-answer-label').text()).toBe('Your Answer');
-  expect(addAnswerBlock.find('.add-your-nickname-label').text()).toBe('What is Your Nickname');
-  expect(addAnswerBlock.find('.add-your-email-label').text()).toBe('Your Email');
+  expect(addAnswerBlock.find('.add-your-question-or-answer-label').text()).toBe('Your Answer*');
+  expect(addAnswerBlock.find('.add-your-nickname-label').text()).toBe('What is Your Nickname*');
+  expect(addAnswerBlock.find('.add-your-email-label').text()).toBe('Your Email*');
   expect(addAnswerBlock.find('.add-question-or-answer-submit').text()).toBe('Submit Answer');
 
 });
