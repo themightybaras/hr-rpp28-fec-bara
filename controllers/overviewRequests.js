53da56dd743eb4acd15a0208829a2b96bf268955
Sara Landis <saralandis5555@gmail.com>
2021-06-22 20:11:41 -0400

    Added Overview Module ... need to update with review meta data request

--- controllers/overviewRequests.js
+++ controllers/overviewRequests.js
@@ -5,20 +5,20 @@ const _ = require('underscore');
 const APIKey = require('../config.js');
 const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp';
 
-const getProducts = (req, res) => {
-  axios.defaults.headers.common['Authorization'] = APIKey;
-  // Get related products (returns array of product IDs)
-  axios({
-    method: 'get',
-    url: baseURL + '/products',
-  })
-    .then ((products) => {
-      res.send(products.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+// const getProducts = (req, res) => {
+//   axios.defaults.headers.common['Authorization'] = APIKey;
+//   // Get related products (returns array of product IDs)
+//   axios({
+//     method: 'get',
+//     url: baseURL + req.url
+//   })
+//     .then ((products) => {
+//       res.send(products.data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
 
 const getProductStyles = ((req, res) => {
   axios.defaults.headers.common['Authorization'] = APIKey;
@@ -27,14 +27,43 @@ const getProductStyles = ((req, res) => {
     url: baseURL + req.url
   })
     .then (data => {
-      //console.log(data.data);
+      console.log('REQ URL', req.url);
       res.send(data.data);
     });
-  // res.send('hello');
 });
 
+const getCurrentProduct = (req, res) => {
+  axios.defaults.headers.common['Authorization'] = APIKey;
+
+  axios({
+    method: 'get',
+    url: baseURL + req.url
+  })
+    .then (data => {
+      res.send(data.data);
+    });
+
+
+  // axios({
+  //   method: 'get',
+  //   url: baseURL + req.url
+  // })
+  //   .then((response1) => {
+  //     return axios({
+  //       method: 'get',
+  //       url: baseURL + req.url + '/styles'
+  //     })
+  //       .then((response2) => {
+  //         console.log('EXTEND,', _.extend(response1.data, response2.data));
+  //         res.send (_.extend(response1.data, response2.data));
+  //       });
+  //   });
+};
 
 
 module.exports = {
-  getProducts, getProductStyles
+  getProductStyles, getCurrentProduct
 };
+
+
+// export getProducts
