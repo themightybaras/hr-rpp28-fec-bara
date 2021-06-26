6d78c30e88ba02af1117ae1d6937b1a7ad1025ba
Sara Landis <saralandis5555@gmail.com>
2021-06-26 11:17:42 -0400

    Working on rating breakdown

--- client/src/components/review/RatingBreakdown.jsx
+++ client/src/components/review/RatingBreakdown.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import StarRating from './StarRating.jsx';
 
-const RatingBreakdown = (props) => {
-  // make stateful
-  // hold percentages for each number
+class RatingBreakdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    // holds rating percentages fro each num
+    };
+    console.log(props)
+    // this.getRecommendPercentage = this.getRecommendPercentage.bind(this);
+    // this.getBarPercentages = this.getBarPercentages.bind(this);
 
-  //function that calculates the percentages for the bars and stores them in state
+    // this.getRecommendPercentage(props.reviewMetaData);
+  }
 
-  return (
-    <div>
-      <span id='largeAverageRating'>{props.avgRating}</span>
-      <StarRating rating = {props.avgRating} />
-      {/* pass state for individual numbers here new bar component*/}
-    </div>
-  );
-};
+  // getRecommendPercentage(metaDataObject) {
+  //   var recommendObject = metaDataObject.recommended;
+
+  //   console.log(metaDataObject);
+  // }
+
+  // //function that calculates the percentages for the bars and stores them in state
+  // getBarPercentages(metaDataObject) {
+  //   var ratingsObject = metaDataObject.ratings;
+
+  //   var totalNumReviews = 0;
+  //   var currentNumRating = 0;
+  //   //itterate over props rating counts
+  //   // get total count of ratings
+  //   // divide each nbumb over total
+  //   var ratingBarPercentage = currentNumRating / totalNumReviews;
+  //   // set state for that num
+
+  // }
+
+  render() {
+    return (
+      <div>
+        <span id='largeAverageRating'>{this.props.avgRating}</span>
+        <StarRating rating = {this.props.avgRating} />
+        {/* pass state for individual numbers here new bar component*/}
+      </div>
+    );
+  }
+}
 
 export default RatingBreakdown;
