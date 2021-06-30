0af508293eb497c5be27461f47bf71f1fdd73c67
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-06-30 17:54:30 -0400

    Added click tracker to addToOutfit card

--- client/src/components/related/AddOutfitCard.jsx
+++ client/src/components/related/AddOutfitCard.jsx
@@ -3,8 +3,15 @@ import { GrAdd } from 'react-icons/gr';
 
 const AddOutfitCard = ({addToOutfit}) => {
 
+  let addAndTrack = () => {
+    addToOutfit();
+    console.log('Tracking, widget: ', 'related widget');
+    console.log('Tracking, type: ', 'div');
+
+  };
+
   return (
-    <div className="productcard addoutfit" onClick={addToOutfit}>
+    <div className="productcard addoutfit" onClick={addAndTrack}>
       <div id='addToOutfitIcon'>
         <GrAdd />
       </div>
