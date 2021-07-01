ffd404aaba459f97a9d96c2c799b1621cdaae517
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-06-30 21:52:47 -0400

    Removed mode property from webpack config and put into two npm scripts. Also added initial product info object.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: path.join(__dirname, 'client', 'src', 'App.jsx'),
   output: {
     path: path.join(__dirname, 'client', 'dist'),
