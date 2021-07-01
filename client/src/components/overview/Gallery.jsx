225113dc34a325461758e91c12b928f9d6eded0d
Sara Landis <saralandis5555@gmail.com>
2021-06-30 23:11:06 -0400

    Removed overview chell code, updated readme

--- client/src/components/overview/Gallery.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const gallery = (props) => {
-  return (
-    <div>
-      {props.render(props.products, props.styles, props.current)}
-    </div>
-  );
-};
-export default gallery;
