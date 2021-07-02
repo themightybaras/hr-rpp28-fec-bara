d3359b206fc95399f862fb01c0a20f19a393504b
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-07-02 16:28:12 -0400

    Attempt at addOutfitCard test

--- tests/relatedProductsTests/AddOutfitCard.test.js
+++ tests/relatedProductsTests/AddOutfitCard.test.js
@@ -14,19 +14,3 @@ test('Card to add current product to outfit', () => {
   expect(testAddOutfit.find('.productcard').exists()).toBe(true);
   expect(testAddOutfit.find('.addoutfit').exists()).toBe(true);
 });
-
-// jest.mock('axios');
-
-// test('Clicking add card should call addAndTrack', () => {
-
-//   axios.get.mockResolvedValue();
-//   let mockHandler = jest.fn();
-//   let testAddOutfit = shallow(
-//     <AddOutfitCard addToOutfit={mockHandler} />
-//   );
-//   return testAddOutfit.simulate('click').then(() => {
-//     expect(mockHandler.mock.calls.length).toEqual(1);
-//   }
-//   );
-//   // done();
-// });
